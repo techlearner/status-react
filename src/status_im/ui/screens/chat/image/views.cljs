704a35b3f755010366e4a9c55242bc3ebcb016ff
Gheorghe Pinzaru <feross95@gmail.com>
2020-10-19 16:02:19 +0300

    Add pick multiple images

--- src/status_im/ui/screens/chat/image/views.cljs
+++ src/status_im/ui/screens/chat/image/views.cljs
@@ -5,7 +5,8 @@
             [reagent.core :as reagent]
             [quo.components.animated.pressable :as pressable]
             [re-frame.core :as re-frame]
-            [status-im.ui.components.colors :as colors]))
+            [quo.design-system.colors :as colors]
+            [quo.core :as quo]))
 
 (defn take-picture []
   (react/show-image-picker-camera #(re-frame/dispatch [:chat.ui/image-captured (.-path %)]) {}))
@@ -24,20 +25,38 @@
      [react/view {:style {:padding 10}}
       [icons/icon :main-icons/gallery]]]]])
 
-(defn image-preview [uri first? panel-height]
-  (let [wh (/ (- panel-height 8) 2)]
-    [react/touchable-highlight {:on-press #(re-frame/dispatch [:chat.ui/camera-roll-pick uri])}
-     [react/image {:style  (merge {:width            wh
-                                   :height           wh
-                                   :background-color :black
-                                   :resize-mode      :cover
-                                   :border-radius    4}
-                                  (when first?
-                                    {:margin-bottom 8}))
-                   :source {:uri uri}}]]))
+(defn image-preview [uri all-selected first? panel-height]
+  (let [wh       (/ (- panel-height 8) 2)
+        selected (get all-selected uri)]
+    [react/touchable-highlight {:on-press #(if selected
+                                             (re-frame/dispatch [:chat.ui/image-unselected uri])
+                                             (re-frame/dispatch [:chat.ui/camera-roll-pick uri]))}
+     [react/view {:style (merge {:width         wh
+                                 :height        wh
+                                 :border-radius 4
+                                 :overflow      :hidden}
+                                (when first?
+                                  {:margin-bottom 4}))}
+      [react/image {:style  (merge {:width            wh
+                                    :height           wh
+                                    :background-color :black
+                                    :resize-mode      :cover
+                                    :border-radius    4})
+                    :source {:uri uri}}]
+      (when selected
+        [react/view {:style {:position         :absolute
+                             :top              0
+                             :bottom           0
+                             :left             0
+                             :right            0
+                             :padding          10
+                             :background-color (:highlight @colors/theme)
+                             :align-items      :flex-end}}
+         [quo/radio {:value true}]])]]))
 
 (defview photos []
   (letsubs [camera-roll-photos [:camera-roll-photos]
+            selected [:chats/sending-image]
             panel-height (reagent/atom nil)]
     [react/view {:style     {:flex           1
                              :flex-direction :row}
@@ -45,11 +64,11 @@
      (let [height @panel-height]
        (for [[first-img second-img] (partition 2 camera-roll-photos)]
          ^{:key (str "image" first-img)}
-         [react/view {:margin-left 8}
+         [react/view {:margin-left 4}
           (when first-img
-            [image-preview first-img true height])
+            [image-preview first-img selected true height])
           (when second-img
-            [image-preview second-img false height])]))]))
+            [image-preview second-img selected false height])]))]))
 
 (defview image-view []
   {:component-did-mount (fn []
